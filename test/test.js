598999be6847ebfb107735216ab5d3fc51082ee9
KylieNamugga <96869130+KylieNamugga@users.noreply.github.com>
2022-05-24 16:32:37 +0300

    recursion

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const factorial = require('../factorial');
+
+describe('Test on the factorial function',() =>{
+    it('should get factorial of 0 as 1', () => {
+        assert.equal(factorial(0),1)
+    })
+
+    it('should get factorial of 1 as 1', () => {
+        assert.equal(factorial(1),1)
+    })
+
+    it('should get factorial of 2 as 2', () => {
+        assert.equal(factorial(2),2)
+    })
+
+    it('should get factorial of 3 as 6', () => {
+        assert.equal(factorial(3),6)
+    })
+
+    it('should get factorial of 4 as 24', () => {
+        assert.equal(factorial(4),24)
+    })
+
+    it('should get factorial of 5 as 120', () => {
+        assert.equal(factorial(5),120)
+    })
+})
